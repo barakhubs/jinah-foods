44b8dd5fb86fdc8fd2269e275c278dfcf25fe582
barakhubs <markbrightbaraka22@gmail.com>
2023-09-14 16:40:37 +0300

    first push

---
+++ resources/js/config/env.js
@@ -0,0 +1,7 @@
+const ENV = {
+    API_URL: 'https://admin.jinahonestop.com',
+    API_KEY: '00TdJKnn5Ck8kWHNd/BuZkVvZD8JPsNSSeMVdSveZ+0=',
+    GOOGLE_MAP_KEY: process.env.MIX_GOOGLE_MAP_KEY,
+    DEMO: process.env.MIX_DEMO
+};
+export default ENV;
