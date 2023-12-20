bc9635438a6f82dd3ee669168baca6f6dc535d69
Baraka Mark Bright <markbrightbaraka22@gmail.com>
2023-12-20 09:18:25 +0300

    changed map api key

--- resources/js/config/env.js
+++ resources/js/config/env.js
@@ -1,7 +1,7 @@
 const ENV = {
     API_URL: 'http://127.0.0.1:8000',
     API_KEY: '00TdJKnn5Ck8kWHNd/BuZkVvZD8JPsNSSeMVdSveZ+0=',
-    GOOGLE_MAP_KEY: process.env.MIX_GOOGLE_MAP_KEY,
+    GOOGLE_MAP_KEY: 'AIzaSyBBaQo7_sF2xk3uNIyKp_Z-4BbaTebGGa4',
     DEMO: process.env.MIX_DEMO
 };
 export default ENV;
